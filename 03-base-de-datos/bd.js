25cbfaa282ae4d2dfcc56ec7eed4ab986ad50b7e
goezheidi16 <goezheidi16@gmail.com>
2025-11-07 17:08:19 -0500

    inicio de maquetación
    inicio de maquetación

---
+++ 03-base-de-datos/bd.js
@@ -0,0 +1,33 @@
+let comic = {
+  "nombreComic": "Andrea Echeverri",
+  "numeroCapitulos": "3",
+  "genero": "....",
+  "sipnosis": "...",
+  "autores": ["Maria Isabel", "Heidi"],
+  "portadaComic": "",
+  "year": 2025,
+  "capitulos": [
+    {
+        "id": 1,
+      "nombre": "Capítulo 1",
+     "personajes": ["..", "...", "...."],
+     "portada": "",
+     "descripcion": "..."
+    },
+    {
+   "id": 2,
+      "nombre": "Capítulo 2",
+     "personajes": ["..", "...", "...."],
+     "portada": "",
+     "descripcion": "..."
+      
+    },
+    {
+      "id": 3,
+      "nombre": "Capítulo 3",
+     "personajes": ["..", "...", "...."],
+     "portada": "",
+     "descripcion": "..."
+    }
+  ]
+}
