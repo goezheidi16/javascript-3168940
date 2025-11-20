a7d87269422105d5d066d5aebdfee064d5b162f6
goezheidi16 <goezheidi16@gmail.com>
2025-11-20 17:12:32 -0500

    Avance, información del comic en la base de datos
    Avance, información del comic en la base de datos

--- 03-base-de-datos/bd.js
+++ 03-base-de-datos/bd.js
@@ -1,26 +1,41 @@
 let comic = {
-  "nombreComic": "TITULO DEL COMIC",
+  "nombreComic": "RUISEÑOERA",
   "numeroCapitulos": "3",
   "genero": "Fantasia",
-  "sinopsis": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam obcaecati eius voluptatem minus mollitia tenetur impedit quo nemo ipsa fuga nesciunt eaque distinctio excepturi magni praesentium maxime corrupti expedita repudiandae itaque, libero, repellat est! Reiciendis, cumque quisquam amet aut excepturi blanditiis consequuntur architecto. Nemo nobis temporibus deserunt rem vitae. Architecto quibusdam ea enim harum reprehenderit facere ut possimus porro.",
+  "sinopsis": "Andrea vive en Bogotá creando su propio camino que pronto se torna dificil cuando la casa de sus padres se incendia y descubre que no fue un accidente cuando una criatura extraña se va en su contra y ella no sabe cuales son sus razones o quién es él, no tardará mucho para descubrir cuál es la realidad de lo que está viviendo y el poder que ella tiene..",
   "autores": ["Maria Isabel", "Heidi"],
   "portadaComic": "",
   "year": 2025,
 
   "personajes":[
     {
-      "nombre":"Andrea Echeverri joven",
-      "descripción":"hgusfgfdgdjgfdguetfdgubdbgfghghghhgghhgghhghfhghghggbgfvbgbgvbhfdhbjdratatata",
+      "nombre":"Andrea joven",
+      "descripción":"25 años, personalidad alegre y diferenciada, ojos cafe oscuro, cabello castaño medio, estatura promedio, conflexión delgada, toca la guitarra, tiene un estilo único, estudiante de bellas artes, cabello hasta mitad de espalda",
       "imagen":"./imagen/perso1.jpg"
     },
     {
-      "nombre":"Andrea Echeverri joven",
-      "descripción":"hgusfgfdgdjgfdguetfdgubdbgfghghghhgghhgghhghfhghghggbgfvbgbgvbhfdhbjdvxn",
+      "nombre":"Andrea actual",
+      "descripción":"50 años, es mamá, alegre, personalidad alegra y diferenciada, ojos cafe oscuro, cabello castaño medio con canas, estatura promedio, conflexión delgada, solista, tiene un estilo único, cabello encima de los hombros",
       "imagen":"./imagen/perso2.jpg"
     },
     {
       "nombre":"Emoción",
-      "descripción":"hgusfgfdgdjgfdguetfdgubdbgfghghghhgghhgghhghfhghghggbgfvbgbgvbhfdhbjdvxn",
+      "descripción":"Alto, oscuro, cuerpo de barro, frustrado y enojado",
+      "imagen":"./imagen/perso3.jpg"
+    },
+    {
+      "nombre":"Amparo de Echeveri",
+      "descripción":"hdhddfjgdgdhfjgfsdnjhdfyjggff",
+      "imagen":"./imagen/perso1.jpg"
+    },
+    {
+      "nombre":"Enrrique Echeverri",
+      "descripción":"gdhfjgkhgjhgdthfyjgukhilkgjfhngfgdtfy",
+      "imagen":"./imagen/perso2.jpg"
+    },
+    {
+      "nombre":"Hector Buitrago",
+      "descripción":"Altura promedio, conflexión delgada, cabello castaño oscuro, trigueño, punketo",
       "imagen":"./imagen/perso3.jpg"
     }
 
@@ -29,24 +44,24 @@ let comic = {
     {
      "id": 1,
      "nombre": "Capítulo 1",
-     "personajes": ["..", "...", "...."],
+     "personajes": ["Andrea joven", "Enrique Echeverri", "Amparo de Echeverri"],
      "portada": "",
-     "descripcion": "..."
+     "descripcion": "Andrea cuenta un poco de cómo fue su vida en casa de sus padres y al ingresar a la universidad descubre que su amor por las bellas artes es más grande de lo que antes creía."
     },
     {
      "id": 2,
      "nombre": "Capítulo 2",
-     "personajes": ["..", "...", "...."],
+     "personajes": ["Andrea joven", "Héctor Buitrago"],
      "portada": "",
-     "descripcion": "..."
+     "descripcion": "Andrea conoce a un chico del cual se enamora y él de ella, se van a vivir juntos y forman una banda. un tiempo después Andrea recibe una llamada de sus padres..."
       
     },
     {
      "id": 3,
      "nombre": "Capítulo 3",
-     "personajes": ["..", "...", "...."],
-     "portada": "",
-     "descripcion": "..."
+     "personajes": ["Andrea joven", "Enrrique Echeverri", "Amparo de Echeverri", "Emoción"],
+     "portada": "imagen/portadaCap.jpg",
+     "descripcion": "La casa de los padres de Andrea se incendió y acude a ayudarlos, descubre que el incendio fue provocado intencionalmente por alguien... o algo."
     }
   ]
 }
